6cdb066b0e01e1f91df5a1a9d1397b111e8fe3a9
Martin Dardis <martindardis96@gmail.com>
2018-05-16 22:45:46 -0300

    TP1 [WIP]- Syntax_validation release

---
+++ tp1/syntax_val/syntax.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define _POSIX_C_SOURCE 200809L
+#define APOSTROFE '''
+#define CORCHETE_ABIERTO '['
+#define CORCHETE_CERRADO ']'
+#define PARENTESIS_ABIERTO '('
+#define PARENTESIS_CERRADO ')'
+
+bool comprobar_linea(char* buffer);
+void imprimir_resultado(bool estado);
+bool comprobar_apostrofe(char* linea,size_t* i);
+bool comprobar_corchetes(char* linea, size_t* i);
+bool comprobar_parentesis(char* linea,size_t* i);
+
+
+int main(int argc,char* argv[]){
+    char* buffer = NULL;
+    size_t cantidad = 0;
+    ssize_t leidos = 0;
+    while( (leidos = getline(&buffer,&cantidad,stdin)) > 0){
+      bool balanceado = comprobar_linea(buffer);
+      imprimir_resultado(balanceado);
+      free(buffer);
+      cantidad = 0;//Limpio buffer
+      buffer = NULL;//Limpio buffer
+    }
+    return 0;
+}
+
+bool comprobar_linea(char* linea){
+  bool abierto = false;
+  size_t i = 0;
+  for( i ; linea[i] != '\0';i++){
+    if (linea[i] == APOSTROFE ){
+      if(!comprobar_apostrofe(linea,&i))
+        return false;
+    }
+    if (linea[i] == CORCHETE_ABIERTO){
+      if(!comprobar_corchetes(linea,&i))
+        return false;
+    }
+    if (linea[i] == PARENTESIS_ABIERTO){
+      if(!comprobar_parentesis(linea,&i))
+        return false;
+    }
+  }
+  return true;
+}
+
+bool comprobar_apostrofe(char* linea,size_t* i){
+  (*i)++;
+  while (linea[*i] != APOSTROFE){
+    if (linea[*i] == '\0')
+      return false;
+    (*i)++;
+  }
+  return true;
+}
+
+bool comprobar_corchetes(char* linea, size_t* i){
+  (*i)++;
+  while (linea[*i] != CORCHETE_CERRADO){
+    if (linea[*i] == '\0')
+      return false;
+    if(linea[*i] == APOSTROFE){
+      if(!comprobar_apostrofe(linea,i))
+        return false;
+    }
+    if(linea[*i] == PARENTESIS_ABIERTO){
+      if(!comprobar_parentesis(linea,i))
+        return false;
+    }
+    (*i)++;
+  }
+  return true;
+}
+
+bool comprobar_parentesis(char* linea,size_t* i){
+  (*i)++;
+  while (linea[*i] != CORCHETE_CERRADO){
+    if (linea[*i] == '\0')
+      return false;
+    if(linea[*i] == APOSTROFE){
+      if(!comprobar_apostrofe(linea,i))
+        return false;
+    }
+    if(linea[*i] == CORCHETE_ABIERTO){
+      if(!comprobar_corchetes(linea,i))
+        return false;
+    }
+    (*i)++;
+  }
+  return true;
+}
+
+void imprimir_resultado (bool estado){
+  if(estado)
+    printf("OK\n");
+  else
+    printf("ERROR\n");
+  return;
+}
