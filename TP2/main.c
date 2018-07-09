d09c1bff92c84321edec19d37e965d37d2bb9f4d
Martin Dardis <martindardis96@gmail.com>
2018-07-09 20:33:20 -0300

    tp2 [OK] - minor main changes

--- TP2/main.c
+++ TP2/main.c
@@ -13,18 +13,29 @@
 #define ERROR -1
 #define CANT_PARAM_INCORRECTO "Cantidad de parametros invalida"
 #define PARAM_INVALIDO "Parametro Incorrecto"
+
+#define ARGC 2
+#define MEM_ARG 1
+
+#define COMM 0
+#define PARAM_1 1
+#define PARAM_2 2
+#define COM_SORT 1
+#define COM_ADD 2
+#define COM_VIEW 3
+#define COM_ERR 0
 /**************************** FUNCIONES INTERNAS ******************************/
 size_t analizar_comando(char** command);
 void print_result(bool state,char* command);
 /*****************************************************************************/
 
 int main (int argc,char* argv[]){
-    if(argc != 2){
+    if(argc != ARGC){
         fprintf(stderr, "%s\n",CANT_PARAM_INCORRECTO);
         return ERROR;
     }
 
-    const int max_memory = atoi(argv[1]);
+    const int max_memory = atoi(argv[MEM_ARG]);
     if(max_memory == 0){
         fprintf(stderr, "%s\n",PARAM_INVALIDO);
         return ERROR;
@@ -41,17 +52,17 @@ int main (int argc,char* argv[]){
     size_t size = 0;
     while(!error_command && getline(&buffer,&size,stdin)>0 ){
         char** command = split(buffer,' ');
-        switch (analizar_comando(command)){
+        switch (analizar_comando(command[COMM])){
             case 1:
-                last_file = strndup(command[2], strlen(command[2])-1);
-                error_command = ordenar_archivo(command[1],last_file,max_memory);
+                last_file = strndup(command[PARAM_2], strlen(command[PARAM_2])-1);
+                error_command = ordenar_archivo(command[PARAM_1],last_file,max_memory);
                 break;
             case 2:
-                last_file = strndup(command[1], strlen(command[1])-1);
+                last_file = strndup(command[PARAM_1], strlen(command[PARAM_1])-1);
                 error_command = agregar_archivo(last_file, &abb);
                 break;
             case 3:
-                error_command = ver_visitantes(abb,command[1],command[2]);
+                error_command = ver_visitantes(abb,command[PARAM_1],command[PARAM_2]);
                 break;
             default:
                 error_command = true;
@@ -61,7 +72,7 @@ int main (int argc,char* argv[]){
             free(last_file);
             last_file = NULL;
         }
-        print_result(error_command,command[0]);
+        print_result(error_command,command[COMM]);
         free_strv(command);
         free(buffer);
         buffer = NULL;
@@ -72,33 +83,14 @@ int main (int argc,char* argv[]){
     return 0;
 }
 
-size_t analizar_comando(char** command){
-    size_t selec = 0;
-    size_t contador_parametros = 0;
-    while(command[contador_parametros]){
-        contador_parametros++;
-    }
-    if( strcmp(command[0],ORDENAR) == 0){
-        if(contador_parametros == 3){
-            selec = 1;
-        } else {
-            selec = 4;
-        }
-    }
-    else if (strcmp(command[0],AGREGAR) == 0){
-        if(contador_parametros == 2){
-            selec = 2;
-        } else {
-            selec = 4;
-        }
-    }
-    else if (strcmp(command[0],VER) == 0){
-        if(contador_parametros == 3){
-            selec = 3;
-        } else {
-            selec = 4;
-        }
-    }
+size_t analizar_comando(char* command){
+    size_t selec = COM_ERR;
+    if( strcmp(command,ORDENAR) == 0)
+            selec = COM_SORT;
+    else if (strcmp(command,AGREGAR) == 0)
+            selec = COM_ADD;
+    else if (strcmp(command,VER) == 0)
+            selec = COM_VIEW;
     return selec;
 }
 
