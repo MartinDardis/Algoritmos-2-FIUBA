279faa837a776d5768958fdc393fddd352eb2cde
Gonzalo Diaz Mulligan <gonzalodiazmulligan@gmail.com>
2018-07-11 19:09:53 -0300

    tp3 [tp3.py] - fix camino minimo

--- TP3/tp3.py
+++ TP3/tp3.py
@@ -1,36 +1,51 @@
 
 def orden_topologico(grafo):
     visitados = set()
+    grados = {}
     pila = Pila()
     for v in grafo.vertices():
-        if v not in visitados:
-            orden_topologico_dfs(grafo, v, pila, visitados)
+        grados[v] = grafo.grado_entrada(v)
+    for v in grafo.vertices():
+        if v not in visitados and grados[v] == 0:
+            orden_topologico_dfs(grafo, v, pila, visitados, grados)
     return pila_a_lista(pila)
 
-def orden_topologico_dfs(grafo, v, pila, visitados):
+def orden_topologico_dfs(grafo, v, pila, visitados, grados):
+    pila.apilar(v)
     visitados.agregar(v)
     for w in grafo.adyacentes(v):
         if w not in visitados:
-            orden_topologico_dfs(grafo, w, pila, visitados)
-    pila.apilar(v)
+            grados[w] -= 1
+            if grados[w] == 0:
+                orden_topologico_dfs(grafo, w, pila, visitados)
 
-def camino_minimo(grafo, origen):
+def camino_minimo(grafo, origen, fin):
     dist = {}
     padre = {}
     for v in grafo.vertices():
-        dist[v] = 9999 #infinito
+        dist[v] = 99999 #infinito
     dist[origen] = 0
     padre[origen] = none
-    q = heap_crear()
-    q.encolar[origen, dist[origen]]
+    q = Heap()
+    q.encolar(origen)
     while not q.esta_vacia():
         v = q_desencolar()
         for w in grafo.adyacentes(v):
-            if dist[v]+grado.peso_arista(v, w) < dist[w]
+            if dist[v] + grafo.peso_arista(v, w) < dist[w]
                 padre[w] = v
-                dist[w] = dist[v]+grafo.peso_arista(v,w)
-                q.encolar(w, dist[w])
-    return padre, dist
+                dist[w] = dist[v] + grafo.peso_arista(v,w)
+                q.encolar(w)
+
+    pila = Pila()
+    v = fin
+    while v != inicio:
+        pila.apilar(v)
+        v = padre[v]
+
+    lista = []
+    for i in range len(pila)
+        lista.append[pila.desapilar()]
+    return lista
 
 def mst_prim(grafo, inicio):
     visitados = set()
@@ -60,7 +75,7 @@ def viajante_backtracing(grafo, inicio):
 
 def viajante_backtracing_recursivo(grafo, inicio, v, visitados, peso, minimo, pila, resultados):
     visitados.append(v)
-    for w in v.adyacentes:
+    for w in grafo.adyacentes(v):
         if w not in visitados:
             peso += peso_arista(v, w)
             if peso < minimo:
@@ -90,7 +105,7 @@ def viajante_greedy(grafo, inicio):
     while v not in todos_visitados:
         visitados.append(v)
         minimo = 99999 #infinito
-        for w in v.adyacentes():
+        for w in grafo.adyacentes(v):
             if peso_arista(v, w) < minimo:
                 minimo = peso_arista(v, w)
                 vertice = w
